3a026cb5a5d94a4d60526a97995f68bc662aa0a2
LaNadia <setsias@gmail.com>
2023-04-04 15:00:56 +0400

    fix: styles

--- src/app/pages/buttons/buttons.component.ts
+++ src/app/pages/buttons/buttons.component.ts
@@ -32,26 +32,4 @@ otherButtons: string =
 <button mat-fab>fab</button>
 <button mat-mini-fab>mini-fab</button>`;
 
-
-// safeHTML(button: any) {
-//   return this.sanitizer.bypassSecurityTrustHtml(button);
-// }
-
-
-  //  '<button mat-button>mat-button</button>',
-  //  '<button mat-raised-button>raised-button</button>',
-  //  '<button mat-flat-button>flat-button</button>',
-  //  '<button mat-stroked-button>Basic</button>',
-  //  '<button mat-stroked-button color="primary">Primary</button>',
-
-  //  '<button mat-icon-button><mat-icon>open_in_new</mat-icon></button>',
-  //  '<button mat-fab>fab</button>',
-  //  '<button mat-mini-fab>mini-fab</button>',
-
-  
-  //  "<button color='primary' mat-button>primary</button>",
-  //  "<button color='accent' mat-button>accent</button>",
-  //  "<button color='warn' disableRipple mat-button>warn</button>",
-  //  "<button color='primary' mat-raised-button>raised-button</button>"
-  
 }
